6e6f55fc538a62d5bb49689bb841ee398d8e946e
svarunid <arun119019@gmail.com>
2020-09-04 18:27:31 +0530

    Started shop app�ð�p🚀

---
+++ node_shop_app_pratice/routes/admin.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/add-product", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+});
+
+router.post("/add-products", (req, res, next) => {
+  res.redirect("/");
+});
+
+module.exports = router;
