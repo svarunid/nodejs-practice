d0b185f355d968956948bee3cfea3e11a4269f39
svarunid <arun119019@gmail.com>
2020-09-07 13:05:42 +0530

    Moved to MVC Pattern

---
+++ node_shop_app_pratice/controllers/products.js
@@ -0,0 +1,22 @@
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect("/");
+};
+
+exports.getProducts = (req, res, next) => {
+  res.render("shop", {
+    prods: Product.fetchAll(),
+    pageTitle: "Shop",
+    path: "/",
+  });
+};
