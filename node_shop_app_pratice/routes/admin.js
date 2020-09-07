b015a9243a2c6c6da8b3c6aa4fac915f3d3910a2
svarunid <arun119019@gmail.com>
2020-09-07 10:40:54 +0530

    Changed to dynamic content with EJS

--- node_shop_app_pratice/routes/admin.js
+++ node_shop_app_pratice/routes/admin.js
@@ -2,14 +2,17 @@ const path = require("path");
 
 const express = require("express");
 
-const rootDir = require("../util/path");
+// const rootDir = require("../util/path");
 
 const router = express.Router();
 
 const products = [];
 
 router.get("/add-product", (req, res, next) => {
-  res.render("add-product", { pageTitle: "Add Product" });
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+  });
 });
 
 router.post("/add-product", (req, res, next) => {
